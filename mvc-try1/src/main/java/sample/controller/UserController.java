a425037120c9e255d6a5409afd08fdc8fa655632
長谷川裕一 <hase@starlight-storm.com>
2018-01-08 17:19:40 +0900

    mvc-answer0をmvc-try0に変更。mvc-try1を追加。

---
+++ mvc-try1/src/main/java/sample/controller/UserController.java
@@ -0,0 +1,22 @@
+package sample.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class UserController {
+
+	@GetMapping("/")
+	public String listUploadedFiles() {
+		return "inputUser";
+	}
+
+	@PostMapping("/input")
+	public String inputNameByPost(@RequestParam String name) {
+		System.out.println("***" + name);
+		return "inputUser";
+	}
+
+}
