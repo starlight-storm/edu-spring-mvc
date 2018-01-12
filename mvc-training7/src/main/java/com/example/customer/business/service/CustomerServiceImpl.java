5a5808244dd12d2436a0cdf71ea9f2db7f26130c
長谷川裕一 <hase@starlight-storm.com>
2018-01-12 10:31:19 +0900

    プロジェクト名の変更.

--- mvc-training7/src/main/java/com/example/customer/business/service/CustomerServiceImpl.java
+++ mvc-training7/src/main/java/com/example/customer/business/service/CustomerServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.customer.business.domain.Customer;
+import com.example.customer.business.repository.CustomerRepository;
 import com.example.customer.common.exception.DataNotFoundException;
 
 @Service
---
+++ mvc-training8-answer/src/main/java/com/example/customer/business/repository/CustomerRepository.java
@@ -0,0 +1,23 @@
+package com.example.customer.business.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import com.example.customer.business.domain.Customer;
+
+@Mapper
+public interface CustomerRepository {
+	
+	@Select("SELECT ID, NAME, ADDRESS, EMAIL_ADDRESS, MEMBERSHIP_TYPE, EMAIL_NOTIFICATION FROM CUSTOMER")
+    public List<Customer> findAll();
+
+	@Select("SELECT * FROM CUSTOMER WHERE ID = #{id}")
+    public Customer findById(int id);
+
+	@Update("UPDATE CUSTOMER SET NAME=#{name}, ADDRESS=#{address}, EMAIL_ADDRESS=#{emailAddress}, MEMBERSHIP_TYPE=#{membershipType}, EMAIL_NOTIFICATION=#{emailNotification} WHERE ID = #{id}")
+    public void update(Customer customer);
+
+}
